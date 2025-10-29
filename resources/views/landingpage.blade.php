dc7fa913870019a51d0c7a7f90652b1fb05ef387
unknown <Naimahqu>
2025-10-29 20:27:57 +0700

    web-pengaduan

---
+++ resources/views/landingpage.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Tittt...Titttt..Hello I'm Alaska</h1>
+    <img src="https://i.pinimg.com/1200x/e1/0d/cc/e10dcca3180e3dc5be4c4b8491f906a5.jpg" alt="">
+</body>
+</html>
